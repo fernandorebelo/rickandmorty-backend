0daf977b8098c8f249361b6cef2bde445bf0c60d
Fernando Rebelo <fernandorebelo.dev@gmail.com>
2023-07-19 20:02:04 -0300

    feat: sql search optimized

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Connection, or_, select
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/RickAndMorty'
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class User(db.Model):
     __tablename__ = 'character'
@@ -20,35 +21,27 @@ class User(db.Model):
     image = db.Column(db.String(100), nullable=False)
 
 
-    def json(self):
-        return {
-            'id': self.id,
-            'name': self.name, 
-            'status': self.status,
-            'species': self.species, 
-            'type_char': self.type_char,
-            'gender':self.gender,
-            'origin_name':self.origin_name,
-            'location_name':self.location_name,
-            'image':self.image
-            }
 
-def get_characters_name(name, page):
+class CharacterSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "status", "species", "type_char", "gender", "origin_name", "location_name", "image")
 
-    #se estiver na pagina 1 -> offset = (valorPagina - 1) * valorLim
-    limit = 20
-    offset = (int(page) - 1) * limit
-    query = User.query.filter(or_(User.name.ilike('%{}%'.format(name)))).order_by(User.id.asc()).limit(limit).offset(offset)
-    results = [user.json() for user in query]
-    return results
 
+character_schema = CharacterSchema(many=True)
 
 
 @app.route('/character', methods=['GET'])
 def character():
     char_name = request.args.get('name', '')
-    char_page = request.args.get('page', 0)
-    return jsonify(get_characters_name(char_name, char_page)), 200
+    char_page = request.args.get('page', 1)
+
+    query = User.query.filter(User.name.ilike(f'%{char_name}%')).order_by(User.id.asc()).paginate(page = int(char_page), per_page = 20)
+
+    return jsonify({
+        'page' : query.page,
+        'total_pages' : query.pages,
+        'results' : character_schema.dump(query.items)
+    }), 200
   
 
 
