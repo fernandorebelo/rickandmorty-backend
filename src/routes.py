47979af6e6d9b0e882a816c0ef1dd2af22575abf
Fernando Rebelo <fernandorebelo.dev@gmail.com>
2023-08-19 18:08:29 -0300

    feat: improved character search

--- src/routes.py
+++ src/routes.py
@@ -1,21 +1,103 @@
 from flask import Blueprint, request, jsonify
 from src.database import character_schema, User
 from flask_cors import CORS, cross_origin
+from src.constants.http_status_codes import *
+import math
+#from flasgger import swag_from
 
 character_bp = Blueprint('character_bp', __name__)
 CORS(character_bp)
 
 @character_bp.route('/', methods=['GET'])
 @cross_origin()
+#@swag_from('./docs/search.yaml')
 def character():
-    print('teste')
-    char_name = request.args.get('name', '')
-    char_page = request.args.get('page', 1)
+    try:
+        char_name = request.args.get('name', '')
+        char_page = request.args.get('page', 1)
 
-    query = User.query.filter(User.name.ilike(f'%{char_name}%')).order_by(User.id.asc()).paginate(page = int(char_page), per_page = 20)
+        try:
+            char_page = int(char_page)
+        except ValueError:
+            search_result = {
+                'data':{
+                    'success': False,
+                    'error': 'Invalidpage number. Please enter a valid integer'
+                },
+                'http_code': HTTP_400_BAD_REQUEST
+            }
+        
+            return jsonify(search_result.get('data')), HTTP_400_BAD_REQUEST
+        
+        character_count = User.query.filter(User.name.ilike(f'%{char_name}%')).count()
 
-    return jsonify({
-        'page' : query.page,
-        'total_pages' : query.pages,
-        'results' : character_schema.dump(query.items)
-    }), 200
+        if not character_count:
+            search_result = {
+                'data':{
+                    'success': False,
+                    'error': 'No characters found with the given page.'
+                },
+                'http_code': HTTP_404_NOT_FOUND
+            }
+
+        total_pages = math.ceil(character_count/20)
+
+        if(char_page>total_pages):
+            search_result = {
+                'data': {
+                    'success': False,
+                    'error': 'Page number out of range.'
+                },
+                'http_code': HTTP_404_NOT_FOUND
+            }
+            return jsonify(search_result.get('data')), HTTP_404_NOT_FOUND 
+
+        query = User.query.filter(User.name.ilike(f'%{char_name}%')).order_by(User.id.asc()).paginate(page = int(char_page), per_page = 20)
+
+        query_data = character_schema.dump(query.items)
+
+        search_result = {
+            'data': {
+                    'success' : True,
+                    'page' : query.page,
+                    'total_pages' : query.pages,
+                    'results' : query_data
+                },
+                'http_code': HTTP_200_OK
+        }
+
+        return jsonify(search_result.get('data')), HTTP_200_OK
+    
+    except Exception as e:
+        raise
+
+def character_by_id(character_id=None):
+    try:
+        
+        character = User.query.get(character_id)
+
+        if not character:
+            search_result = {
+                'data':{
+                    'success': False,
+                    'error': 'Character not found.'
+                },
+                'http_code': HTTP_404_NOT_FOUND
+            }
+
+            return jsonify(search_result.get('data')), HTTP_404_NOT_FOUND
+
+        character_data = character_schema.dump(character)
+
+        search_result = {
+            'data': {
+                    'success' : True,
+                    'character' : character_data,
+                },
+                'http_code': HTTP_200_OK
+        }
+
+        return jsonify(search_result.get('data')), HTTP_200_OK
+    
+    except Exception as e:
+        raise
